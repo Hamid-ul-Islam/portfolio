389532423c943417d267ddb0659a8acb8c14c00d
Hamidul Islam <hamidthedev@gmail.com>
2024-06-28 20:21:55 +0600

    review card fix

--- components/testimonials.tsx
+++ components/testimonials.tsx
@@ -22,7 +22,7 @@ export function AnimatedPinDemo({
   company: string;
 }) {
   return (
-    <div className="!w-full flex items-center justify-center dark:text-white sm:px-0 px-20">
+    <div className="!w-full flex items-center justify-center dark:text-white text-gray-700 sm:px-0 px-20">
       <PinContainer title="Link to profile" href={link} className="!w-full !h-full">
         <div className="flex basis-full flex-col  tracking-tight text-slate-100/50 sm:basis-1/2 w-full h-full ">
           <div className="flex gap-2 items-center">
